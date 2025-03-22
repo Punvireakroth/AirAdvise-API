ff8342de72681466e86421140aea9bfcf5ba1ec0
Pun VireakRoth <vireakrothpun@gmail.com>
2025-03-22 22:16:06 +0700

    👍 User preference

--- app/Http/Controllers/API/UserPreferenceController.php
+++ app/Http/Controllers/API/UserPreferenceController.php
@@ -3,9 +3,42 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Traits\ApiResponses;
 use Illuminate\Http\Request;
+use App\Http\Resources\UserPreferenceResource;
+use App\Http\Requests\UserPreferenceRequest;
 
 class UserPreferenceController extends Controller
 {
-    //
+    use ApiResponses;
+
+    public function show(Request $request)
+    {
+        $preferences = $request->user()->preference;
+
+        if (!$preferences) {
+            // Create default preferences if none exist
+            $preferences = $request->user()->preference()->create([]);
+        }
+
+        return $this->success(new UserPreferenceResource($preferences));
+    }
+
+
+    public function update(UserPreferenceRequest $request)
+    {
+        $user = $request->user();
+        $preferences = $user->preference;
+
+        if (!$preferences) {
+            $preferences = $user->preference()->create($request->validated());
+        } else {
+            $preferences->update($request->validated());
+        }
+
+        return $this->success(
+            new UserPreferenceResource($preferences),
+            'Preferences updated successfully'
+        );
+    }
 }
